f5c31d21589072eab974bb0a5fcd8bcd720e8ebf
anshika-om <ao3051@srmist.edu.in>
2026-04-02 10:43:47 +0530

    level1

---
+++ LEVEL3/DigitFrequency.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0) {
+            count++;
+            temp /= 10;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+
+        int[] freq = new int[10];
+
+        for (int i = 0; i < count; i++) {
+            freq[digits[i]]++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + freq[i] + " times");
+            }
+        }
+
+        sc.close();
+    }
+}
